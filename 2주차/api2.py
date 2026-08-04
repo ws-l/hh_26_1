399d877c9b38393532e860c55766863b483ce84b
ws-l <108631181+ws-l@users.noreply.github.com>
2026-08-04 16:55:04 +0900

    Add files via upload

--- 2주차/api2.py
+++ 2주차/api2.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 import pandas as pd
+import pickle
 df = pd.read_csv("binary.csv")
+
+with open("test.pkl", "rb") as f:
+    saved = pickle.load(f)
+arr = saved["pipe1"].fit_transform(df) #pipeline
+df = pd.DataFrame(arr, columns = df.columns)
 
 app = FastAPI()
 @app.get("/data")
